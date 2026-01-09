7ccad65ae19fbc3d6ab3d6101473fb006d8731cb
Matt Cuvanov <42915392+cuvam@users.noreply.github.com>
2026-01-09 12:30:46 -0500

    add tempparse.c

---
+++ tempparse.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+#define WIDTH 40
+#define MINTEMP 46
+#define MAXTEMP 52
+
+#define DIST_MINTEMP 42
+#define DIST_MAXTEMP 56
+#define DIST_WIDTH (DIST_MAXTEMP - DIST_MINTEMP + 1)
+
+int main() {
+    char *filename = "/path/to/thermal.log";
+
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        printf("Failed to open %s\n", filename);
+        return 1;
+    }
+
+    int buckets[24] = {0};
+    int bucketsize[24] = {0};
+    int numsamples = 0;
+
+    int otherbuckets[24][DIST_WIDTH] = {0};
+
+    char *line = NULL;
+    unsigned long len;
+    long num_bytes;
+    while ((num_bytes = getline(&line, &len, fp)) != EOF) {
+        char timestamp[512];
+        int hour;
+        int minute;
+        int second;
+        int fan;
+        int pkgtemp;
+        int core0temp;
+        int core1temp;
+        int parsed = sscanf(line, "%511s %d:%d:%d Fan:%d Pkg:+%d.0C Core0:+%d.0C Core1:+%d.0C",
+                timestamp, &hour, &minute, &second, &fan, &pkgtemp, &core0temp, &core1temp
+        );
+        if (parsed < 8) {
+            printf("failed to parse line (%i) %s\n", parsed, line);
+        } else {
+            buckets[hour] += pkgtemp;
+            bucketsize[hour]++;
+            numsamples++;
+            int idx = pkgtemp - DIST_MINTEMP;
+            if (idx < 0) idx = 0;
+            if (idx >= DIST_WIDTH) idx = DIST_WIDTH - 1;
+            otherbuckets[hour][idx]++;
+        }
+    }
+    fclose(fp);
+
+    printf("Temperature breakdown, %i samples (#=%.3fC, min %.2fC, max %.2fC):\n\n", numsamples, 1.0/(float)(MAXTEMP-MINTEMP), (float)MINTEMP, (float)MAXTEMP);
+
+    for (int i = 0; i < 24; i++) {
+        printf("%02i |", i);
+        float avg;
+        if (bucketsize[i] > 0) {
+            avg = (float)buckets[i] / (float)bucketsize[i];
+            int scaled = ((float)(avg - MINTEMP)/(float)(MAXTEMP-MINTEMP))*WIDTH;
+            if (scaled < 0) scaled = 0;
+            if (scaled > WIDTH) scaled = WIDTH;
+            printf(" %.2fC ", avg);
+            for (int j = 0; j < scaled; j++)
+                putchar('#');
+        }
+        printf("\n");
+    }
+
+    printf("\n\nDistribution per hour:\n\n");
+
+    char brightness[] = { '.', ':', ',', '\\', '\'', '-', '^', '=', '*', '+', '?', '!', '|', '#', 'X', '%', 'W', 'M', '@' };
+    int brange = sizeof(brightness);
+
+    printf("    ");
+    for (int j = DIST_MINTEMP; j <= DIST_MAXTEMP; j++)
+        printf("%d", j % 10);
+    printf("\n");
+
+    for (int i = 0; i < 24; i++) {
+        int highestBucket = 0;
+        for (int j = 0; j < DIST_WIDTH; j++) {
+            if (otherbuckets[i][j] > highestBucket)
+                highestBucket = otherbuckets[i][j];
+        }
+        printf("%02i: ", i);
+        if (highestBucket > 0) {
+            for (int j = 0; j < DIST_WIDTH; j++) {
+                putchar(brightness[(otherbuckets[i][j] * (brange - 1)) / highestBucket]);
+            }
+        }
+        printf("\n");
+    }
+}
